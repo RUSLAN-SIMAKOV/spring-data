e57562337bf259210d3cf122ecff0b828803af64
Ruslan Simakov <ua667766706@gmail.com>
2021-08-27 13:41:04 +0200

    Initial commit

---
+++ src/test/java/ruslan/simakov/SpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package ruslan.simakov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
